78a09d7be7be44adeb23654d322608669ecbfb9f
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-01-11 11:27:29 +0530

    Create half_adder.v

---
+++ half_adder/half_adder.v
@@ -0,0 +1,10 @@
+`timescale 1ns / 1ps
+
+module half_adder( input a, b , 
+                   output sout,cout);
+                   
+                   
+                   assign sout = a^b;
+                   assign cout =a&b;
+
+endmodule
