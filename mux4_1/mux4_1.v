7490194e8ecb7b1db1494fc1f78ef465851b683f
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-01-18 11:49:03 +0530

    Update mux4_1.v

--- mux4_1/mux4_1.v
+++ mux4_1/mux4_1.v
@@ -9,12 +9,12 @@ module mux4_1 (input d0,
 
   wire p,q,r,s ,s1bar, s0bar ;
 
-  NOT(s1bar , s1);
-  NOT(s0bar, s0);
-  AND(p,s1bar,s0bar,d0);
-  AND(q,s1bar,s0,d1);
-  AND(r,s1,s0bar,d2);
-  AND(s,s1,s0,d3);
-  OR (y,p,q,r,s);
+  not(s1bar , s1);
+  not(s0bar, s0);
+ and(p,s1bar,s0bar,d0);
+ and(q,s1bar,s0,d1);
+  and(r,s1,s0bar,d2);
+  and(s,s1,s0,d3);
+ or(y,p,q,r,s);
 
 endmodule
