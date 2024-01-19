e4f1c011cb5314cd50469a732c38b848d5d2c44b
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-01-19 10:46:23 +0530

    Create comparator.v

---
+++ comparator/comparator.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ps
+module comparator(input a,
+                 input b,
+                 output g,
+                 output s,
+                  output e);
+
+  always@ (a,b)
+    begin
+      if(a==0 & b==0)
+        e=1;
+      else if(a==0 & b==1 )
+      s=1;
+      else if (a ==1 & b==0)
+        g=1;
+      else(a==1 & b==1);
+      e=1;
+    end
+
+endmodule
+      
+    
