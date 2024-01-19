48ea1ca44c1046de2d57dd4ed30534cea6ce729a
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-01-19 10:47:25 +0530

    Update mux2_1.v

--- mux_2*1/mux2_1.v
+++ mux_2*1/mux2_1.v
@@ -1,8 +1,9 @@
 `timescale 1ns / 1ps
 module mux2_1 (input a, input b, input s, 
               output y);
-  always@(a or b or s )
-    begin 
-      assign y = (~s & a ) | (s & b );
-    end 
+  wire sbar,p,q;
+  not(sbar,s);   
+  and(p,a,sbar);
+  and(q,b,s);       
+  or(y,p,q);
 endmodule
