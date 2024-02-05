324e0b2771edf3d2caf45969df05fd79e650a650
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-02-05 14:26:45 +0530

    Update Decoder2to4_withenable.v

--- Decoder2to4_withenable/Decoder2to4_withenable.v
+++ Decoder2to4_withenable/Decoder2to4_withenable.v
@@ -1,25 +1,21 @@
 `timescale 1ns/1ps
-module Decoder2to4_withenable(input e, 
-                              input a, 
-                              input b, 
-                              output reg[3:0] y) ;
-
-  always@(e, a, b) ;
-
-  begin
-    if(e==1) 
-      begin
-        if( a==0 & b==0) 
-          y=4'b1000 ;
-        else if (a==0 & b==1) 
-          y=4'b0100;
-        else if (a==1 & b==0) 
-          y=4'b0010;
-        else
-          y=4'b0001 ;
-      end
-else 
-  y= 4'b0000;
-  end
-
-endmodule 
+module decoder24_behaviour(en,a,b,y);
+   
+   input en,a,b;
+   output reg [3:0]y;
+   
+   always @(en,a,b)
+     begin
+       
+       if(en==0)
+         begin
+           if(a==1'b0 & b==1'b0) y=4'b1110;
+           else if(a==1'b0 & b==1'b1) y=4'b1101;
+           else if(a==1'b1 & b==1'b0) y=4'b1011;
+           else if(a==1 & b==1) y=4'b0111;
+           else y=4'bxxxx;
+         end
+       else
+        y=4'b1111;
+     end
+endmodule
