8cb72dffad08b085e93470c0248439634a5fcf4f
sneha2901varshney <156281059+sneha2901varshney@users.noreply.github.com>
2024-03-04 13:13:21 +0530

    Create testbench.v

---
+++ T Flip flop using SR, D and JK flip flop/testbench.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module testbench;
+reg clk,rst,t;
+wire q;
+  
+  t_flip_flop dut(t,clk,rst,q);
+  
+  initial 
+  begin 
+       clk=0;
+       t=0;
+       forever #4 clk=~clk;
+  end
+    
+  initial 
+      begin
+          rst=1;
+          #10;
+          rst=0;
+          forever
+          begin   
+          #10 t = 1'b1;
+          #20  t = 1'b0; 
+          end
+      end
+    initial begin
+    $monitor("\t clock: %b  T: %b  Q: %b",clk,t,q);
+    #100$finish;
+    end
+endmodule
